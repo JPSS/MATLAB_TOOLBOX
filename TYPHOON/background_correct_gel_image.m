8862619f3be023d1e304e9dd3581f03bef9cc93a
Jean-Philippe Sobczak <>
2019-02-06 15:54:28 +0100

    use non-linear bins for background histogram in background_correct_gel_image()
    typhoon image data has higher sensitivity at lower intensities, use non-linear (quadratic size) bin sizes to match this

--- TYPHOON/background_correct_gel_image.m
+++ TYPHOON/background_correct_gel_image.m
@@ -43,11 +43,38 @@ for i = 1:imageData.nrImages
     %subtract maximum location value of smoothed image histogram from image
     if histogram_background_bool
         
-        %highest pixel value in current image
-        max_pixel_value = max(max(imageData.images{i}));
+        % check if .gel format was used to generate LAU format
+        if isfield(imageData,'images_tiff_format')
+            % image data should be in LAU format
+            
+            %smallest and largest pixel values that are possible in LAU format
+            min_pixel_value = 0.01976423;
+            max_pixel_value = 1976.076364;
+            % factors for transforming from linear to square-root format
+            multiplier = 65535 / (sqrt(max_pixel_value) - sqrt(min_pixel_value));
+            offset = sqrt(min_pixel_value) * multiplier; 
+        else
+            %smallest and largest pixel value in current image
+            min_pixel_value = min(min(imageData.images{i}));
+            max_pixel_value = max(max(imageData.images{i}));
+            % factors for transforming from linear to square-root format
+            multiplier = 65535 / (sqrt(max_pixel_value) - sqrt(min_pixel_value));
+            offset = sqrt(min_pixel_value) * multiplier; 
+        end
+        
+        %number of bins in histogram
+        nr_steps = 2^16;
+
+        % calculate quadratically increasing bin sizes for image intensity histogram that is sensitive at low intensities
+        % skip very first datapoint in case there is a 0 intensity background in gel-free areas
+        edges = (((1:nr_steps-1) + offset) ./ multiplier).^2;
         
         %calculate histogram of image
-        [histogram, edges] = histcounts(imageData.images{i}, 2^16);
+        [histogram, edges] = histcounts(imageData.images{i}, edges);
+        
+        % normalize histogram with bin sizes to calculate intensity probability density
+        histogram = histogram ./ (edges(2:end) - edges(1:end - 1));
+        
         %smooth histogram of image
         histogram_smooth = smoothdata(histogram, 'movmean', histogram_smooth_span);
         [~, loc] = max(histogram_smooth);
