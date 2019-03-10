fb772c332f13c0c49576013ae8f54ee7e09852a4
jonasfunke <jonasfunke@gmail.com>
2019-03-10 10:03:34 +0100

    manual_lane_selection: added option to pass on number of lanes

--- TYPHOON/private/manual_lane_selection.m
+++ TYPHOON/private/manual_lane_selection.m
@@ -1,4 +1,4 @@
-function [ lanes ] = manual_lane_selection( image, pos )
+function [ lanes ] = manual_lane_selection( image, pos, varargin )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
     
@@ -8,14 +8,25 @@ area = image( pos(2):pos(2)+pos(4), pos(1):pos(1)+pos(3)); % get the image insid
     horizontalProfile = sum(area); % integrate along vertical (y-axis) 
 
 
-    tmp = inputdlg('Number of lanes?', 'Number of lanes', 1, {'1'});
-    N_lanes = str2double(tmp{1});
+    if isempty(varargin)     
+        tmp = inputdlg('Number of lanes?', 'Number of lanes', 1, {'1'});
+        N_lanes = str2double(tmp{1});
+    else
+        if varargin{1}>0
+            N_lanes = varargin{1};
+        else
+            tmp = inputdlg('Number of lanes?', 'Number of lanes', 1, {'1'});
+            N_lanes = str2double(tmp{1});
+        end
+    end
+        
     xy = zeros(N_lanes+1, 2);
     
     cf = figure;
     hold all
     plot(x, horizontalProfile)
     set(gca, 'Xlim', [min(x), max(x)])
+    title('Select intial lanes.')
     for i=1:N_lanes+1
         [xy(i,1), xy(i,2)] = ginput(1);
         vline(xy(i,1), 'r');
